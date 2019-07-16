4aee223a864190a36b7e959b4ff963e4ebe502c2
izarudnyi <ivan.zarudnyi@leverate.com>
2019-07-16 13:12:52 +0300

    Db schema

--- db/migrations/20190716124326_create_officers_table.js
+++ db/migrations/20190716124326_create_officers_table.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('officers', function(table) {
+    table.increments().unsigned().primary();
+    table.string('name').notNull();
+    table.timestamps(true, true);
+  });
+
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('officers');
 };
---
+++ db/migrations/20190716124335_create_cases_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cases', function(table) {
+    table.increments().unsigned().primary();
+    table.string('title').notNull();
+    table.string('state').notNull().defaultTo('NEW').index();
+    table.integer('officer_id').index().references('id').inTable('officers').onDelete('SET NULL');
+    table.string('description').notNull();
+    table.timestamps(true, true);
+  });
+
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('cases');
+};
